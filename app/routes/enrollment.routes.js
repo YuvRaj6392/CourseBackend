66a2f3305e1790f20d3b18bcaa2e47dce64011a0
Yuv Raj <yuvraj.srivastava6392@gmail.com>
2022-09-20 16:30:44 +0530

    Added a project

---
+++ app/routes/enrollment.routes.js
@@ -0,0 +1,21 @@
+const auth = require("../../middleware/auth"); 
+
+module.exports = app => {
+  const enrollments = require("../controllers/enrollment.controller");
+  
+    var router = require("express").Router();
+  
+    router.post("/enroll", enrollments.enroll);
+    
+    app.use('/api', auth, router);
+  };
+
+  /*
+    Enrollment
+    URL -> http://localhost:3000/api/enroll
+    body -> {
+    "userId" : "6069b434e81fc334e8c7b8d3",
+    "courseId" : "606f395167a42d2f501eb1c5"
+    }
+
+  */
